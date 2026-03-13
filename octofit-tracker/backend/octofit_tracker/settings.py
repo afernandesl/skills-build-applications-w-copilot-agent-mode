1cc130f6e916273e2af38db4d2072afbbfc089e5
afernandesl <155015346+afernandesl@users.noreply.github.com>
2026-03-13 14:21:35 +0000

    Adiciona campos de grupos e permissões ao modelo de usuário, ajusta ALLOWED_HOSTS para suportar ambientes de desenvolvimento e atualiza a raiz da API com URLs dinâmicos

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-_8l)m95cex(w=z4_1yp5@td=4wazrtent6+sdrssj=*7wsig(2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
